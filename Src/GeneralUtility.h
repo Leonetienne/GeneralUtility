2958c2c72baec469523c69e60ac1c8ef09f712a8
Leonetienne <leonetienne@hotmail.de>
2022-02-27 13:48:21 +0100

    Initial commit

---
+++ Src/GeneralUtility.h
@@ -0,0 +1,12 @@
+#ifndef GENERALUTILITY_GENERALUTILITY_H
+#define GENERALUTILITY_GENERALUTILITY_H
+
+class GeneralUtility {
+public:
+
+private:
+    // No instanciation! >:(
+    GeneralUtility();
+};
+
+#endif //GENERALUTILITY_GENERALUTILITY_H
